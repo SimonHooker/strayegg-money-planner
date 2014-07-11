d3a8c8962706062da2cf582b7bfde28c31f1ab9f
Simon Hooker <simon@mso.net>
2014-07-11 14:39:55 +0100

    Added a simple server.js

---
+++ server.js
@@ -0,0 +1,21 @@
+ var app = express();
+ 
+/* serves main page */
+app.get('/', function(req, res) {
+    res.sendfile('index.html')
+});
+
+app.post("/user/add", function(req, res) {
+	/* some server side logic */
+	res.send("OK");
+});
+
+/* serves all the static files */
+app.get(/^(.+)$/, function(req, res){
+	console.log('static file request : ' + req.params);
+	res.sendfile( __dirname + req.params[0]);
+});
+
+app.listen(80, function() {
+	console.log("Listening on " + port);
+});
