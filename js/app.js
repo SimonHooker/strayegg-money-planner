aaaebd8e20657886052eba11b5b5d7762947b686
Simon Hooker <simon@mso.net>
2014-07-11 17:00:28 +0100

    Simple route config

--- js/app.js
+++ js/app.js
@@ -1,11 +1,12 @@
 App = Ember.Application.create();
 
 App.Router.map(function() {
-  // put your routes here
+	// put your routes here
+	this.route('new');
 });
 
 App.IndexRoute = Ember.Route.extend({
-  model: function() {
-    return ['red', 'yellow', 'blue'];
-  }
+	model: function() {
+		return ['red', 'yellow', 'blue'];
+	}
 });
